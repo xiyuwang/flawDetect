24c586f3933bd7521574e2af17366490fbf8dc51
Xiyu Wang <wang.xy@hotmail.com>
2017-12-07 17:58:54 +0800

    add controller for static file navigation

---
+++ detectionServer/src/main/java/com/detect/controller/navigateController.java
@@ -0,0 +1,13 @@
+package com.detect.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Created by Administrator on 2017/12/07.
+ */
+@Controller
+@EnableAutoConfiguration
+public class navigateController {
+}
